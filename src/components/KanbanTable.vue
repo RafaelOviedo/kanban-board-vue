513b1845dfb53adc1241656c08846dd236255311
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-05-24 19:10:23 -0300

    update: adding structure for movie cards display

--- src/components/KanbanTable.vue
+++ src/components/KanbanTable.vue
@@ -1,29 +1,54 @@
 <template>
   <div id="kanban-table-component">
     <div id="titles-container" ref="titlesContainer" @scroll="followColumnsScroll">
-      <div id="title-box" v-for="item in categoryColumns" :key="item.id">
-        {{ item.column_name }}
+      <div id="title-box" v-for="column in categoryColumns" :key="column._id">
+        {{ column.column_name }}
       </div>
     </div>
     <div id="columns-container" ref="columnsContainer" @scroll="followTitlesScroll">
-      <div id="title-column" v-for="item in categoryColumns" :key="item.id">
-        {{ item.column_name }}
+      <div id="title-column" v-for="column in categoryColumns" :key="column._id">
+        <div v-if="!reactiveState.movieCards[column.column] || reactiveState.movieCards[column.column].length === 0">
+          <span>No cards to show</span>
+        </div>
+        <div v-else>
+          <div v-for="movieCard in reactiveState.movieCards[column.column]" :key="movieCard._id">
+            <p>{{ movieCard.name }}</p>
+            <p>{{ movieCard.category }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, reactive } from 'vue';
 import axios from 'axios';
 
+const reactiveState = reactive({ movieCards: {} });
+
+const titlesContainer = ref(null);
+const columnsContainer = ref(null);
 const categoryColumns = ref([]);
-const titlesContainer = ref(null)
-const columnsContainer = ref(null)
 
 const getCategoryMovieColumns = async () => {
   const response = await axios('/movie_category_columns');
   categoryColumns.value = response.data;
+  getFilteredMovieCards();
+}
+
+const getFilteredMovieCards = () => {
+  categoryColumns.value.forEach(async (column) => {
+    const response = await axios(`/movies?category=${column.column}`);
+  
+    for (const cardsSet in reactiveState) {
+      if (Object.prototype.hasOwnProperty.call(Object, reactiveState.movieCards, cardsSet)) {
+        return;
+      } else {
+        reactiveState.movieCards[column.column] = response.data;
+      }
+    }
+  });
 }
 
 const followTitlesScroll = () => {
